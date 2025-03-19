91f6d1da37ba265d3fc82d5fe5c876178c0f60d2
Muralii Krishnan <muraliikrishnant@gmail.com>
2025-03-19 18:34:17 -0400

    Save user with role reference compatible with Firestore

--- app/data/entity/Users.ts
+++ app/data/entity/Users.ts
@@ -1,12 +1,11 @@
-import { getOwnerId } from "./Roles";
-import { addDoc, collection, getDocs, query, where } from "@react-native-firebase/firestore";
+import { getOwnerDoc, getOwnerId } from "./Roles";
+import { addDoc, collection, getDoc, doc, query, where } from "@react-native-firebase/firestore";
 import { FIREBASE_DB } from "../../../config/Firebase";
-import { DB_ROLES, DB_ROLES_OWNER } from "../model/Constants";
 import { User } from "./../model/Types";
 
 export const saveUser = async (user: User): Promise<boolean> => {
-    const owenerId = await getOwnerId();
-    const { deviceId, ...userData } = { ...user, role: owenerId };
+    const owner = await getOwnerDoc();
+    const { deviceId, ...userData } = { ...user, role: owner?.ref };
     try {
         const savedUser = await addDoc(collection(FIREBASE_DB(), "users"), userData);
         return true;
@@ -14,5 +13,4 @@ export const saveUser = async (user: User): Promise<boolean> => {
         console.error("Error saving user : " + user.email, error);
         throw error;
     };
-    return false;
 };
